77c2ebdf7f12135224fc2f5fcf9c52ffb2999b61
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2018-10-24 10:59:39 +0100

    PDF's Have been Completed

---
+++ Morgan Hill Zombie shooter/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Rigidbody2D Body2D;
+    void Start()
+    {
+        
+        Body2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Body2D.velocity = new Vector2(x, y) * speed;
+        Body2D.angularVelocity = 0.0f;
+    }
+}
