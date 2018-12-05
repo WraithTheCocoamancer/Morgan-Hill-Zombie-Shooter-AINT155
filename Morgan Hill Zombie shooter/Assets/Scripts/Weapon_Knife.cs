44e838c94005c4bf439bf3066904bf990e097287
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2018-12-05 12:05:28 +0000

    Final Weeks of session
    knife now damages on click To do animations

---
+++ Morgan Hill Zombie shooter/Assets/Scripts/Weapon_Knife.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Weapon_Knife : MonoBehaviour
+{
+    //public GameObject SlashPrefab;
+    //public Transform[] SlashSpawn;
+
+    public Collider2D col;
+
+    public float fireTime = 0.5f;
+    private bool isFiring = false;
+
+    void Start()
+    {
+        col.enabled = false;
+    }
+
+    private void SetFiring()
+    {
+        isFiring = false;
+        col.enabled = false;
+    }
+    private void Fire()
+    {
+        isFiring = true;
+
+        col.enabled = true;
+
+        //for (int i = 0; i < SlashSpawn.Length; i++)
+        //{
+        //    Instantiate(SlashPrefab, SlashSpawn[i].position, SlashSpawn[i].rotation);
+        //}
+        //if (GetComponent<AudioSource>() != null)
+        //{
+        //    GetComponent<AudioSource>().Play();
+        //}
+        Invoke("SetFiring", fireTime);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+    }
+}
